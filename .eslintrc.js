bb37a4e166d877604e6fbd735be941ecda8eb0b0
주상 <jusang@jusang-ui-Macmini.local>
2024-12-20 09:39:26 +0900

    feat: 전역 색상 시스템 구현
    - shared/config/colors.ts 파일에 전역 색상 정의
    - shared/lib/useColors.ts 훅 생성으로 색상 사용 편의성 향상

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,6 @@ module.exports = {
   ],
   extends: [
     '@react-native',
-    'plugin:@tanstack/eslint-plugin-query/recommended'
+    'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
-  rules: {
-    "@tanstack/query/exhaustive-deps": "error",
-    "@tanstack/query/no-deprecated-options": "error",
-    "@tanstack/query/prefer-query-object-syntax": "error",
-    "@tanstack/query/stable-query-client": "error"
-  }
 };
---
+++ src/shared/lib/useColors.ts
@@ -0,0 +1,5 @@
+import { colors, Colors } from '../config/colors';
+
+export const useColors = (): Colors => {
+  return colors;
+};
